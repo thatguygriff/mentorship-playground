5e0bb0c04a35fb9c9ccdad5de76a5a4aefe867f5
James Griffin <james@unsupervised.ca>
2021-04-17 15:59:11 -0300

    Use cache to track job count
    Signed-off-by: James Griffin <james@unsupervised.ca>

--- internal/pkg/jobs/worker.go
+++ internal/pkg/jobs/worker.go
@@ -2,21 +2,38 @@ package jobs
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 
 	"github.com/suborbital/reactr/rt"
 )
 
+const workerKey = "genericWorkerCount"
+
 type timed struct{}
 
 // Run runs a generic job
 func (t timed) Run(job rt.Job, ctx *rt.Ctx) (interface{}, error) {
-	time.Sleep(time.Second * 30)
+	time.Sleep(time.Second * 5)
+
+	count := 0
+
+	countBytes, err := ctx.Cache.Get(workerKey)
+	if err == nil && countBytes != nil {
+		s := string(countBytes)
+		count, err = strconv.Atoi(s)
+		if err != nil {
+			fmt.Println(countBytes)
+		}
+	}
+	count++
 
 	j := job.Data().(*Job)
 	j.Result = &Result{
-		Message: fmt.Sprintf("Finished job at %s", time.Now().String()),
+		Message: fmt.Sprintf("Finished job #%d at %s", count, time.Now().String()),
 	}
+
+	ctx.Cache.Set(workerKey, []byte(fmt.Sprintf("%d", count)), 0)
 
 	return fmt.Sprintf("finished %s", job.String()), nil
 }
